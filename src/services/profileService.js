a106155c02c8c39c9326e6c9a44f81b2b15ca5df
n001ce <a1ccormier@gmail.com>
2021-08-13 17:39:21 -0400

    update app.jsx

---
+++ src/services/profileService.js
@@ -0,0 +1,38 @@
+import * as tokenService from "./tokenService"
+const BASE_URL = "/api/profiles/"
+
+export function getUserProfile() {
+  return fetch(`${BASE_URL}userProfile`, 
+  {headers: { Authorization: "Bearer " + tokenService.getToken() }},
+  {mode: 'cors'})
+  .then(res => res.json())
+}
+
+export function getAllProfiles() {
+  return fetch(`${BASE_URL}`, 
+  {headers: { Authorization: "Bearer " + tokenService.getToken() }},
+  {mode: 'cors'})
+  .then(res => res.json())
+}
+
+export function friend(id) {
+  return fetch(
+    `${BASE_URL}/friend/${id}`,
+    {
+      method: 'PATCH',
+      headers: { Authorization: "Bearer " + tokenService.getToken() }
+    },
+    { mode: "cors" }
+    ).then((res) => res.json())
+}
+
+export function unfriend(id) {
+  return fetch(
+    `${BASE_URL}/unfriend/${id}`,
+    {
+      method: 'PATCH',
+      headers: { Authorization: "Bearer " + tokenService.getToken() }
+    },
+    { mode: "cors" }
+    ).then((res) => res.json())
+}
