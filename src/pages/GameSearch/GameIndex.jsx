caa76bac3edbfc2acc3adeebcf5d141459611c4c
Kyler Larson <klarson1016@gmail.com>
2021-08-16 08:26:35 -0700

    game search results tyling

--- src/pages/GameSearch/GameIndex.jsx
+++ src/pages/GameSearch/GameIndex.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import * as mediaAPI from '../../services/mediaService'
 import GameCard from '../../components/GameCard/GameCard'
+import MyProfileBar from '../../components/MyProfileBar/MyProfileBar'
+import {Box, Grid, Typography} from '@material-ui/core';
+
 
 class GameSearch extends Component {
   state = {
     searchResults: []
   }
+
+  //const UserLanding = ({userProfile}) => { 
+    
+  
 
   async componentDidMount() {
     const { params } = this.props.match
@@ -28,14 +35,23 @@ class GameSearch extends Component {
   render() { 
     return (
       <>
-        <h1>Game Results</h1>
-        {this.state.searchResults.map(game =>
+      <MyProfileBar userProfile={this.props.userProfile} />
+      <Box ml={35} mr={5} my={3}>
+      <Typography variant={'h4'} >Game Results</Typography>
+      <Box my={3}>
+        <Grid container spacing={3}>
+        {this.state.searchResults.map(game => 
+        <Grid item xs={12} s={6} md={4} lg={3} mx={'auto'} >
           <GameCard
-          game={game}
-          key={game.id}
-          userProfile={this.props.userProfile}
-        />
-        )} 
+            game={game}
+            key={game._id}
+            
+          />
+        </Grid>
+        )}
+        </Grid>
+      </Box>
+      </Box>
       </>  
     );
   }
