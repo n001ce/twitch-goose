3c175c08f32aa297bed689661817e3ffefa770f7
n001ce <a1ccormier@gmail.com>
2021-08-17 00:57:32 -0400

    StreamerSearch rendering api pulls

--- src/pages/StreamSearch/StreamSearch.jsx
+++ src/pages/StreamSearch/StreamSearch.jsx
@@ -13,23 +13,34 @@ class StreamSearch extends Component {
   async componentDidMount() {
     const { params } = this.props.match
     const streamResults = await mediaAPI.searchStreams(params.query)
-    this.setState({ streamResults })
+    this.setState({ streamResults : streamResults.data })
   }
 
   async componentDidUpdate(prevProps) {
     const { params } = this.props.match
     if (params.query !== prevProps.match.params.query){
         const streamResults = await mediaAPI.searchStreams(params.query)
-        this.setState({ streamResults })
+        this.setState({ streamResults: streamResults.data })
       }
   }
 
   render() { 
     return (
       <>
-      <h4> Stream Results </h4>
-          
-      </>  
+      <Box my={3}>
+        <Grid container spacing={3}>
+        {this.state.streamResults.map(stream =>
+         <Grid item xs={12} s={6} md={4} lg={3} mx={'auto'} >
+          <StreamerCard
+          stream={stream}
+          key={stream.id}
+          userProfile={this.props.userProfile}
+        />
+         </Grid>
+        )} 
+        </Grid>
+      </Box>
+      </>
     );
   }
 }
